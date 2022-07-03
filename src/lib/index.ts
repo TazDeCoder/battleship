4ee197d0196a6c06c1bed3b0ec5e884d876110f2
Tarwat <t.uddin9121@gmail.com>
2022-07-03 21:05:40 +0100

    Update index entry file
    Add makeData module to list of exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import Ship from './ship/ship';
 import Gameboard from './gameboard/gameboard';
 import Player from './player/player';
+import makeData from './makeData/makeData';
 
-export { Ship, Gameboard, Player };
+export { Ship, Gameboard, Player, makeData };
