4c4c3e889f5a241f6f4a2349c9483d825c4445d8
Tarwat <t.uddin9121@gmail.com>
2022-07-15 15:48:31 +0100

    Add Ships interface
    Refractored object type for storing ships an object

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,10 @@
 export type Coord = [string, number];
+
+export interface Ships {
+  [key: string]: {
+    hits: Coord[];
+    positions: Coord[];
+    isSunk: () => {};
+    hit: ([ltr, num]: Coord) => {};
+  };
+}
