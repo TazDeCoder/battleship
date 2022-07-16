dd5f8aa87ef2b9121d2f6953e2ff184cd4f6e588
Tarwat <t.uddin9121@gmail.com>
2022-07-16 17:00:32 +0100

    Update tests for Board component
    Minor changes to ensure tests function properly and added two tests

--- src/components/Board/__tests__/Board.test.tsx
+++ src/components/Board/__tests__/Board.test.tsx
@@ -1,27 +1,77 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Board from '../Board';
 
-import { Player } from '../../../lib/index';
+import { Player, Ship } from '../../../lib/index';
+
+type Checkbox = HTMLTableCellElement & { disabled: boolean };
 
 const DUMMY_PLAYERS = {
   player1: Player(),
   player2: Player(),
 };
 
+beforeAll(() => {
+  DUMMY_PLAYERS.player1.setPiece(Ship(1), ['A', 1], 'vert');
+  DUMMY_PLAYERS.player1.setPiece(Ship(2), ['C', 9], 'horz');
+  DUMMY_PLAYERS.player1.setPiece(Ship(3), ['E', 4], 'vert');
+  DUMMY_PLAYERS.player1.setPiece(Ship(4), ['F', 6], 'horz');
+  DUMMY_PLAYERS.player1.setPiece(Ship(5), ['D', 5], 'vert');
+  DUMMY_PLAYERS.player2.setPiece(Ship(1), ['D', 1], 'vert');
+  DUMMY_PLAYERS.player2.setPiece(Ship(2), ['C', 9], 'horz');
+  DUMMY_PLAYERS.player2.setPiece(Ship(3), ['A', 4], 'vert');
+  DUMMY_PLAYERS.player2.setPiece(Ship(4), ['B', 6], 'horz');
+  DUMMY_PLAYERS.player2.setPiece(Ship(5), ['E', 5], 'vert');
+});
+
 describe('Board component', () => {
-  it('should render a 10x10 board', () => {
-    render(
-      <Board
-        boardData={{
-          missed: DUMMY_PLAYERS.player1.missed,
-          ships: DUMMY_PLAYERS.player1.ships,
-        }}
-        onTogglePiece={() => {}}
-      />
-    );
-    const boardEl = screen.getByTestId('board') as HTMLTableElement;
-    expect(boardEl.rows.length).toBe(10);
+  describe('renders each player board correctly', () => {
+    it('should render a 10x10 board for "player1"', () => {
+      render(
+        <Board
+          boardData={{
+            ships: DUMMY_PLAYERS.player1.ships,
+            missed: [],
+          }}
+          onTogglePiece={() => {}}
+        />
+      );
+      const boardEl = screen.getByTestId('board') as HTMLTableElement;
+      expect(boardEl.rows.length).toBe(10);
+    });
+    it('should render a 10x10 board for "player2"', async () => {
+      render(
+        <Board
+          boardData={{
+            ships: DUMMY_PLAYERS.player2.ships,
+            missed: [],
+          }}
+          onTogglePiece={() => {}}
+        />
+      );
+      const boardEl = screen.getByTestId('board') as HTMLTableElement;
+      await userEvent.click(boardEl);
+      expect(boardEl.rows.length).toBe(10);
+    });
+  });
+  describe('when either player toggles a checkbox there should an appropriate feedback', () => {
+    it('should render a disabled checkbox when toggled for the first time', async () => {
+      render(
+        <Board
+          boardData={{
+            ships: DUMMY_PLAYERS.player2.ships,
+            missed: [],
+          }}
+          onTogglePiece={() => {}}
+        />
+      );
+      const [checkboxEl] = screen.getAllByLabelText(
+        'board piece'
+      ) as Checkbox[];
+      await userEvent.click(checkboxEl);
+      expect(checkboxEl.disabled).toBe(true);
+    });
   });
 });
