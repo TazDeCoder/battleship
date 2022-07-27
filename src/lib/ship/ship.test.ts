2ab005e6d7ba1fb985e72b258a607dcb63c6a8cc
Tarwat <t.uddin9121@gmail.com>
2022-07-27 16:01:33 +0100

    Update ship module and tests
    Updated module and tests to match modified Ship object

--- src/lib/ship/ship.test.ts
+++ src/lib/ship/ship.test.ts
@@ -5,47 +5,84 @@ describe('Ship function', () => {
     it('returns carrier piece', () => {
       const carrier = Ship(1);
       expect(carrier).toMatchObject({
-        name: 'Carrier',
+        name: 'carrier',
         size: 5,
         hits: [],
       });
-      expect(carrier.isSunk()).toBe(false);
     });
     it('returns battleship piece', () => {
       const battleship = Ship(2);
       expect(battleship).toMatchObject({
-        name: 'Battleship',
+        name: 'battleship',
         size: 4,
         hits: [],
       });
-      expect(battleship.isSunk()).toBe(false);
     });
     it('returns cruiser piece', () => {
       const cruiser = Ship(3);
       expect(cruiser).toMatchObject({
-        name: 'Cruiser',
+        name: 'cruiser',
         size: 3,
         hits: [],
       });
-      expect(cruiser.isSunk()).toBe(false);
     });
     it('returns submarine piece', () => {
       const submarine = Ship(4);
       expect(submarine).toMatchObject({
-        name: 'Submarine',
+        name: 'submarine',
         size: 3,
         hits: [],
       });
-      expect(submarine.isSunk()).toBe(false);
     });
     it('returns destroyer piece', () => {
       const destroyer = Ship(5);
       expect(destroyer).toMatchObject({
-        name: 'Destroyer',
+        name: 'destroyer',
         size: 2,
         hits: [],
       });
-      expect(destroyer.isSunk()).toBe(false);
+    });
+  });
+  describe('should return correct ship piece based on ship name', () => {
+    it('returns carrier piece', () => {
+      const carrier = Ship('carrier');
+      expect(carrier).toMatchObject({
+        name: 'carrier',
+        size: 5,
+        hits: [],
+      });
+    });
+    it('returns battleship piece', () => {
+      const battleship = Ship('battleship');
+      expect(battleship).toMatchObject({
+        name: 'battleship',
+        size: 4,
+        hits: [],
+      });
+    });
+    it('returns cruiser piece', () => {
+      const cruiser = Ship('cruiser');
+      expect(cruiser).toMatchObject({
+        name: 'cruiser',
+        size: 3,
+        hits: [],
+      });
+    });
+    it('returns submarine piece', () => {
+      const submarine = Ship('submarine');
+      expect(submarine).toMatchObject({
+        name: 'submarine',
+        size: 3,
+        hits: [],
+      });
+    });
+    it('returns destroyer piece', () => {
+      const destroyer = Ship('destroyer');
+      expect(destroyer).toMatchObject({
+        name: 'destroyer',
+        size: 2,
+        hits: [],
+      });
     });
   });
   describe('should be able to mark positions as "hit" if ship not sunk', () => {
--- src/lib/ship/ship.ts
+++ src/lib/ship/ship.ts
@@ -1,30 +1,27 @@
 import { Coord } from '../../models/index';
+import { SHIPS } from '../../constants';
 
-const ships = {
-  1: {
-    name: 'carrier',
-    size: 5,
-  },
-  2: {
-    name: 'battleship',
-    size: 4,
-  },
-  3: {
-    name: 'cruiser',
-    size: 3,
-  },
-  4: {
-    name: 'submarine',
-    size: 3,
-  },
-  5: {
-    name: 'destroyer',
-    size: 2,
-  },
-};
+const SHIPS_OBJ = SHIPS.reduce((obj: any, [name, size], idx) => {
+  const newObj = { ...obj };
+  newObj[idx + 1] = {
+    name,
+    size,
+  };
+  return newObj;
+}, {});
 
-export default function Ship(shipNumber: keyof typeof ships) {
-  const { name, size } = ships[shipNumber];
+export default function Ship(id: keyof typeof SHIPS_OBJ | string) {
+  let ship;
+  if (typeof id === 'string') {
+    const shipIdx = Object.keys(SHIPS_OBJ).findIndex(
+      (key: string | number) => SHIPS_OBJ[key].name === id
+    );
+    if (shipIdx === -1) throw Error('Invalid ship name');
+    ship = SHIPS_OBJ[shipIdx + 1];
+  } else {
+    ship = SHIPS_OBJ[id];
+  }
+  const { name, size } = ship;
   let hitsIdx = 0;
   const hits: Coord[] = [];
   // Check if ship has 'sunk'
