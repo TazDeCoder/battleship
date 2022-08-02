e736ded61cc3ac9cd1661fa632a10c0740f526cb
Tarwat <t.uddin9121@gmail.com>
2022-08-02 18:36:57 +0100

    Update models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,24 @@
+// Enums
+export enum Dir {
+  'horz',
+  'vert',
+}
+// Types
 export type Coord = [string, number];
-
+// Interfaces
 export interface Ships {
   [key: string]: {
     hits: Coord[];
     positions: Coord[];
     isSunk: () => {};
     hit: ([ltr, num]: Coord) => {};
+  };
+}
+export interface IGameData {
+  [key: string]: {
+    [key: string]: {
+      coord: Coord;
+      dir: keyof typeof Dir;
+    };
   };
 }
